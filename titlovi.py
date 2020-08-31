26bb347f9ed6bae4741771c8e453937970807ac1
Damir Čohadarević <cohadar@gmail.com>
2020-08-31 07:30:06 +0200

    t

---
+++ titlovi.py
@@ -0,0 +1,29 @@
+import sys
+
+
+def main():
+    with open(sys.argv[1], 'rb') as f:
+        data = bytearray(f.read())
+        mapper = {}
+        mapper[138] = 'Š'
+        mapper[142] = 'Ž'
+        mapper[154] = 'š'
+        mapper[158] = 'ž'
+        mapper[200] = 'Č'
+        mapper[230] = 'ć'
+        mapper[232] = 'č'
+        mapper[240] = 'đ'
+        out = ""
+        for c in data:
+            if c < 128:
+                out += chr(c)
+            elif c in mapper:
+                out += mapper[c]
+            else:
+                raise ValueError(c)
+                out += '?'+str(c)
+        print(out)
+
+
+if __name__ == '__main__':
+    main()
